9f7a4f68458fab53af21b483b9cac15647848f03
Dwain Jay <dwain.jay@abbott.com>
2015-09-07 08:54:18 -0700

    Updated code and added screen shots

--- FingerPrintScanner/FingerPrintScanner/ViewController.m
+++ FingerPrintScanner/FingerPrintScanner/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 @import LocalAuthentication;
 
-@interface ViewController ()
+@interface ViewController () {
+    BOOL fingerPrintScannerBusy;
+}
 
 @end
 
@@ -18,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,6 +30,14 @@
 
 
 - (IBAction)fingerPrintScanRequestButtonPressed:(id)sender {
+    
+    
+    // Filter for multiple clicks
+    if (fingerPrintScannerBusy) {
+        return;
+    }
+    
+    fingerPrintScannerBusy = YES;
     
     LAContext *laContext = [[LAContext alloc] init];
 
@@ -39,12 +50,61 @@
 
         // Perform actual finger print prompt
         [laContext evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"Authenticate for use" reply:^(BOOL success, NSError *authenticationError){
+            
+            fingerPrintScannerBusy = NO;
+            
             if (success) {
                 NSLog(@"Fingerprint validated.");
             }
             else {
+
                 NSLog(@"Fingerprint validation failed: %@.", authenticationError.localizedDescription);
+                
+                switch (authenticationError.code) {
+                        
+                    case LAErrorUserFallback : {
+                            NSLog(@"Failed LAErrorUserFallback");
+                        
+                        [self performSelectorOnMainThread:@selector(performLoginOnMain) withObject:nil waitUntilDone:NO];
+                        }
+                        break;
+                        
+                    case LAErrorUserCancel : {
+                            NSLog(@"Failed LAErrorUserCancel");
+                        }
+                        break;
+                        
+                    case LAErrorSystemCancel : {
+                            NSLog(@"Failed LAErrorSystemCancel");
+                        }
+                        break;
+                        
+                    case LAErrorPasscodeNotSet : {
+                            NSLog(@"Failed LAErrorPasscodeNotSet");
+                        }
+                        break;
+                
+                    case LAErrorTouchIDNotAvailable : {
+                            NSLog(@"Failed LAErrorTouchIDNotAvailable");
+                        }
+                        break;
+                        
+                    case LAErrorTouchIDNotEnrolled : {
+                            NSLog(@"Failed LAErrorTouchIDNotEnrolled");
+                        }
+                        break;
+                        
+                    default : {
+                        NSLog(@"Failed Unknown Reason");
+                        }
+                        break;
+           
+                        
+                }
+            
             }
+            
+            
         }];
 
         
@@ -57,9 +117,61 @@
                                               cancelButtonTitle:@"Ok"
                                               otherButtonTitles:nil];
         [alert show];
+        
+        fingerPrintScannerBusy = NO;
 
     }
     
+    
+}
+
+-(void) performLoginOnMain {
+    
+    [self performSelector:@selector(performLogin) withObject:nil afterDelay:0.3];
+
+}
+
+-(void) performLogin {
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Manual Login"
+                                                    message:@"Enter Password"
+                                                   delegate:self
+                                          cancelButtonTitle:@"Cancel"
+                                          otherButtonTitles:@"Login", nil];
+    
+    // General input text
+    //    alertView.alertViewStyle = UIAlertViewStylePlainTextInput;
+    
+    // Password secure input field
+    // alertView.alertViewStyle = UIAlertViewStyleSecureTextInput;
+    
+    // General text (Login) and secure password field
+    alertView.alertViewStyle = UIAlertViewStyleLoginAndPasswordInput;
+    
+    [alertView show];
+
+}
+
+
+#pragma UIAlert Delegate
+
+// This is called when the user enters a PIN and clicks Login or Cancel
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    // The user clikced Cancel
+    if (buttonIndex == 0) {
+        NSLog(@"User Canceled");
+    }
+    
+    // The user clicked Login
+    if (buttonIndex == 1) {
+        // Get the input text
+        NSString *userName = [[alertView textFieldAtIndex:0] text];
+        NSString *password = [[alertView textFieldAtIndex:1] text];
+        
+         NSLog(@"User Logged. User Name: %@ Password: %@", userName, password);
+        
+    }
     
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # iOSFingerPrintScanner
 Sample of iOS finger Print Scanner for iPhone
+
+
+This is the source for an iOS app that uses the Finger Print Scanner for Authentication. It also allows a fallback prompt for user name and password.
+
+You will need XCode to run this program.
+
+All source is open source, you may use this for anything you wish.
+
+There is no intended use, you at your own risk.
+
+![screenshot](https://github.com/ThomasJay/iOSFingerPrintScanner/blob/master/home.png)
+
+
+![screenshot](https://github.com/ThomasJay/iOSFingerPrintScanner/blob/master/auth.png)
+
+![screenshot](https://github.com/ThomasJay/iOSFingerPrintScanner/blob/master/login.png)
